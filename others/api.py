482be5e650f7fc864ab38e30a4aec1e49c08e141
Anoop Christopher Thiparala <30379425+anpct@users.noreply.github.com>
2020-06-27 00:16:15 +0530

    captcha added

--- others/api.py
+++ others/api.py
@@ -1,11 +1,27 @@
 from .serializers import PostRequestSerializer, FeedbackSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny, IsAuthenticated
-
+import requests
+from .util import get_client_ip
+from rest_framework.response import Response
 
 class PostRequestAPIView(generics.CreateAPIView):
     serializer_class = PostRequestSerializer
     permission_classes = [ AllowAny, ]
+
+    def post(self, request, *args, **kwargs):
+        r = requests.post('https://www.google.com/recaptcha/api/siteverify',
+        data={
+            'secret': secret_key,
+            'response': request.data['responseG'],
+            'remoteip': get_client_ip(self.request),
+        }
+        )
+
+    if r.json()['success']:
+        return self.create(request, *args, **kwargs)
+        return Response(data={'error': 'ReCAPTCHA not verified.'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
 
 
 class FeedbackAPIView(generics.CreateAPIView):
