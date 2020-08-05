dbcc4e607b2373a81284b47cd0d150cc4365ea5e
Anoop Christopher Thiparala <30379425+anpct@users.noreply.github.com>
2020-08-05 15:26:49 +0530

    Update settings.py

--- avg/settings.py
+++ avg/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['157.245.101.245', '127.0.0.1', 'localhost', 'avgcdr.tk']
 
