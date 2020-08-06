083b631c87be30c160819feb10c099e2dc810d9d
Anoop Christopher Thiparala <30379425+anpct@users.noreply.github.com>
2020-08-06 21:31:03 +0530

    Update serializers.py

--- master/serializers.py
+++ master/serializers.py
@@ -7,7 +7,7 @@ from rest_framework_tracking.models import APIRequestLog
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email", "first_name", "last_name", "is_superuser"]
+        fields = ["id", "username", "email", "first_name", "last_name", "is_superuser", "password"]
 
 
 class PostSerializer(serializers.ModelSerializer):
